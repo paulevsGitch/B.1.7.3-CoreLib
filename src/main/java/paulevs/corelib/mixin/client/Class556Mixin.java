9c6b12b7804029d3ef1fa1ded317db2a7f91dbad
paulevsGitch <paulevs@yandex.ru>
2021-02-06 11:31:42 +0300

    Compilation fix

--- src/main/java/paulevs/corelib/mixin/client/Class556Mixin.java
+++ src/main/java/paulevs/corelib/mixin/client/Class556Mixin.java
@@ -4,10 +4,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-import net.minecraft.class_556;
+import net.minecraft.client.render.HandItemRenderer;
 import paulevs.corelib.CoreLib;
 
-@Mixin(class_556.class)
+@Mixin(HandItemRenderer.class)
 public class Class556Mixin {
 	@ModifyConstant(method = "method_1862", constant = @Constant(floatValue = 256.0F), expect = 3)
 	private float changeSizeF(float original) {
