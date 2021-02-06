78592a1382616f3f86e2b4ed3799ca5bb1d88ab4
paulevsGitch <paulevs@yandex.ru>
2021-02-06 10:49:31 +0300

    Cleanup

--- src/main/java/paulevs/corelib/CoreLib.java
+++ src/main/java/paulevs/corelib/CoreLib.java
@@ -1,13 +1,7 @@
 package paulevs.corelib;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.ItemType;
-import paulevs.corelib.api.ModelRegistry;
-import paulevs.corelib.model.Model;
-import paulevs.corelib.model.shape.Shape;
-import paulevs.corelib.model.shape.ShapeBoxCustom;
 import paulevs.corelib.texture.TextureAtlas;
-import paulevs.corelib.texture.UVPair;
 
 public class CoreLib implements ModInitializer {
 	public static final int FACE_POS_X = 5;
@@ -26,38 +20,6 @@ public class CoreLib implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		System.out.println("CoreLib Initialized");
-
-		// ModelRegistry.addItemModel(ItemType.stick, new
-		// ModelOBJ("/assets/corelib/test.obj",
-		// "/assets/corelib/lum_icon.png"));
-
-		Model test = new Model() {
-			Shape box = new ShapeBoxCustom();
-
-			@Override
-			public void renderBlock() {
-				Shape.setUV(particleUV());
-				box.render();
-			}
-
-			@Override
-			public void renderItem() {
-				Shape.setUV(particleUV());
-				Shape.setOffsetItem();
-				box.render();
-			}
-
-			@Override
-			public UVPair particleUV() {
-				return this.getUV("texture");
-			}
-
-		};
-		test.addTexture("texture", "/assets/corelib/lum_icon.png");
-		// ModelRegistry.addItemModel(ItemType.stick, new
-		// ModelOBJ("/assets/corelib/test.obj",
-		// "/assets/corelib/lum_icon.png"));
-		ModelRegistry.addItemModel(ItemType.stick, test);
 	}
 
 	public static float getAtlasSize() {
